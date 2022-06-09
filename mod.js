252d43e9ce80ebd08426f2e139a425e855d4dcd3
Aikenpil <gustebas@hotmail.com>
2022-06-08 22:13:44 -0300

    Add mod.js

---
+++ mod.js
@@ -0,0 +1,9 @@
+var document = Document;
+function mod2(){
+
+    let containerMod= document.getElementById("mod");
+    let numero = document.getElementsById("inputmod").value;
+
+    containerMod.innerHTML += numero;
+
+}
