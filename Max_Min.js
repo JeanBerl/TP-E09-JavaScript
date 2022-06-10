4f2c343353363395a01c8850aca2991419dfd987
JeanBerly <jeanberlyy@gmail.com>
2022-06-09 23:41:05 -0300

    AAAA socorro

--- Max_Min.js
+++ Max_Min.js
@@ -1,17 +1,40 @@
 var document = Document;
-function max(){
+
+function max(a, b){
+    if (a == b){
+        return;
+    }
+    return a > b ? a : b;
+}
+
+function min(a, b){
+    if (a == b){
+        return;
+    }
+    return b > a ? b : a;
+}
+
+
+function maxPag(){
 
     let containerMax = document.getElementById("max");
     containerMin.innerHTML = '';
     let a = document.getElementById("input_amax").value;
     let b = document.getElementById("input_bmax").value;
 
-    if(a > b) containerMax.innerHTML += `${a}`;
-    else if(b > a) containerMax.innerHTML += `${b}`;
+    if(a > b){
+        containerMax.innerHTML += `${a}`;
+        return a;
+    }
+    else if(b > a){
+        containerMax.innerHTML += `${b}`;
+        return b;
+    }
     else containerMax.innerHTML += 'Eles sao iguais';
+    return;
 }
 
-function min(){
+function minPag(){
     
     let containerMin = document.getElementById("min");
     containerMin.innerHTML = '';
--- triangulo.js
+++ triangulo.js
@@ -1,14 +1,10 @@
-var document = Document;
 function desenhaTriangulo() {
     let numLinhas = parseInt(document.getElementById("input_triangulo").value) + 1;
-    let containerTriangulo = document.getElementById("desenho_triangulo");
-    let desenhoTriangulo = "";
     for (let i = 0; i < numLinhas; i++) {
-        desenhoTriangulo += "<p>";
+        let desenhoTriangulo = "";
         for (let j = 0; j < i; j++) {
             desenhoTriangulo += "#";
         }
-        desenhoTriangulo += "</p>"
+        console.log(desenhoTriangulo);
     }
-    containerTriangulo.innerHTML = desenhoTriangulo;
 }
